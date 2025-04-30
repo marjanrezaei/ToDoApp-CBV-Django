a753b5b3b3903f098be6cf32853ad8b22c0c8d9d
marjanrezaei <rezaei.marjann@gmail.com>
2025-04-30 12:44:31 +0330

    Define User and UserManager models

--- core/core/settings.py
+++ core/core/settings.py
@@ -140,3 +140,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# user manager config
+AUTH_USER_MODEL = "accounts.User"
+
