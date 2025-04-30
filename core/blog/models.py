815b5375b7cded95c522693d49f33e026e6c1e6c
marjanrezaei <rezaei.marjann@gmail.com>
2025-04-30 10:51:17 +0330

    create Task model

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
+
 # Create your models here.
+class Task(models.Model):
+    '''
+    this is a class to define tasks for todo app
+    '''
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
