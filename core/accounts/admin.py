899aed069a2df8a23c4b64954cf8e9658bd1e769
marjanrezaei <rezaei.marjann@gmail.com>
2025-04-30 13:41:55 +0330

    Define Profile model

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Profile
 # Register your models here.
 
 class CustomUserAdmin(UserAdmin):
@@ -25,3 +25,4 @@ class CustomUserAdmin(UserAdmin):
     )
     
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)
