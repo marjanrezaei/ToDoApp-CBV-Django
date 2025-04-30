8e3b222cd92e48321ebb38ff0dc1ad7920d65a1a
marjanrezaei <rezaei.marjann@gmail.com>
2025-04-30 16:25:16 +0330

    show base page with class base view
    use template view for show base.html

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Task
 
 # Create your views here.
+class BaseView(TemplateView):
+    template_name = 'base.html'
+    
+    def get_context_data(self, **kwargs):
+        """
+        a class based view to show base page.
+        """
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = Task.objects.all()
+        return context
