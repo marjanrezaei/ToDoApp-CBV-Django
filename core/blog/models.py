733e3ea8dace52db45d5b76f237a19f6af7152a5
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-01 14:35:03 +0330

    Give user info with Profile model

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+# from accounts.models import Profile
 
-User = get_user_model()
+# User = get_user_model()
 
 # Create your models here.
 class Task(models.Model):
     '''
     this is a class to define tasks for todo app
     '''
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey('accounts.Profile', on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     description = models.TextField()
     completed = models.BooleanField(default=False)
