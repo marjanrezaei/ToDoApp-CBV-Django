c5b92ec47bc8a0333094c50bc23da47c8dbeca05
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-01 20:01:38 +0330

    inheritance from base.html

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path('base', views.BaseView.as_view(), name='base-view'),
+    path('', views.BaseView.as_view(), name='base-view'),
     path('task/', views.TaskListView.as_view(), name="task-list"),
     path('task/<int:pk>/', views.TaskDetailView.as_view(), name="task-detail"),
     path('task/create/', views.TaskCreateView.as_view(), name="task-create"),
