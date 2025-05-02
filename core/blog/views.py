feb1b8756414ff2a90d5304c34edc6f8e27e8136
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-02 13:09:58 +0330

    Welcome to Login User

--- core/blog/views.py
+++ core/blog/views.py
@@ -3,32 +3,32 @@ from django.views.generic import TemplateView, ListView, DetailView, CreateView,
 from .models import Task
 from .forms import TaskForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from accounts.models import Profile
 
 
 class BaseView(TemplateView):
     template_name = 'base.html'
     
-    def get_context_data(self, **kwargs):
-        """
-        A class-based view to show the base page.
-        """
-        context = super().get_context_data(**kwargs)
-
-        tasks = Task.objects.filter(author__user__email=self.request.user)  # Corrected filter
-        if tasks.exists():  # Prevent errors if queryset is empty
-            context['names'] = tasks.first().author
-        else:
-            context['names'] = None  # Handle case when no tasks exist
-        return context
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            request.session['user_id'] = request.user.id
+            try:
+                profile = Profile.objects.get(user=request.user)
+                request.session['first_name'] = profile.first_name
+            except Profile.DoesNotExist:
+                request.session['first_name'] = ''  # Handle missing profile gracefully
+        
+        return super().dispatch(request, *args, **kwargs)
     
 
 class TaskListView(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
+    paginate_by = 2
 
     def get_queryset(self):
         # Filter queryset to show only tasks for the logged-in user
-        return Task.objects.filter(author__user__email=self.request.user)
+        return Task.objects.filter(author__user__email=self.request.user).order_by('-id')
 
         
     
@@ -45,7 +45,8 @@ class TaskCreateView(LoginRequiredMixin, CreateView):
     success_url = '/task/'
     
     def form_valid(self, form):
-        form.instance.author = self.request.user
+        profile = Profile.objects.get(user=self.request.user)
+        form.instance.author = profile 
         return super().form_valid(form)
         
     
