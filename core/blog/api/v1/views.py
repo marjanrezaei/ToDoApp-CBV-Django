fd5a317837b6a57c2dc3926daff2de7d70df8c55
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-10 12:18:58 +0330

    Define view

---
+++ core/blog/api/v1/views.py
@@ -0,0 +1,24 @@
+
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .serializers import TaskSerializer
+from ...models import Task
+from rest_framework import viewsets
+from .permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .paginations import DefaultPagination
+from .filters import TaskFilter
+
+class TaskModelViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly] 
+    serializer_class = TaskSerializer 
+    queryset = Task.objects.filter(status=True)
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ['title', 'description']
+    ordering_fields = ['published_at']
+    pagination_class = DefaultPagination
+    filterset_class = TaskFilter
+    
+   
+   
+    
