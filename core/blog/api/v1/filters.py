d1ab0d70e209acfdc93ff0b2d627139a72c26cbe
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-10 12:18:46 +0330

    Define filters

---
+++ core/blog/api/v1/filters.py
@@ -0,0 +1,15 @@
+from rest_framework import generics
+from django_filters import rest_framework as filters
+from ...models import Task
+
+class TaskFilter(filters.FilterSet):
+    """Filter for Task model"""
+    class Meta:
+        model = Task
+        fields = {
+            'title': ['exact', 'in'],
+            'author': ['exact'],
+            'completed': ['exact'],
+        }
+        
+        
