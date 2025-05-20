af8bf134883591b3d470569916d2092267da2f54
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-20 19:06:56 +0330

    Set urls

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from . import views
-
 app_name = "accounting"
 
 urlpatterns = [
     path('signup/', views.SignupView.as_view(), name='signup'),
-    path('profile/', views.ProfileUpdateView.as_view(), name='profile_edit'),
+    path('profile-edit/', views.ProfileUpdateView.as_view(), name='profile_edit'),
     path('', include('django.contrib.auth.urls')),
     path('api/v1/', include('accounts.api.v1.urls')),
 
