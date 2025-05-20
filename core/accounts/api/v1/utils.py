d5e679f63aa5c49a736c509907a0396c8726c7b2
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-20 19:11:24 +0330

    defile thread for send email

---
+++ core/accounts/api/v1/utils.py
@@ -0,0 +1,12 @@
+import threading
+
+class EmailThread(threading.Thread):
+    """
+    Thread to send email.
+    """
+    def __init__(self, email_obj):
+        self.email_obj = email_obj
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.email_obj.send()
